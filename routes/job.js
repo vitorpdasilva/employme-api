76fdcd1557cef9fd8bb825d18160c8486b738a8e
vitorboccio <vitorboccio@gmail.com>
2022-09-14 00:06:33 -0700

    eslint

--- routes/job.js
+++ routes/job.js
@@ -1,25 +1,41 @@
 const router = require('express').Router();
 const Job = require('../models/Jobs');
+const User = require('../models/User')
 
 router.route('/job/:id/apply')
   .post(async (req, res) => {
-    console.log({ req: req.body });
+    console.log({ req: req.body, params: req.params, type: typeof req.params.id });
 
-    const { nModified } = await Job.findOne({ id: req.params.id }).updateOne({
-      $addToSet: { applicants: req.body.applicantId },
+    // const { nModified } = await Job.updateOne({ id: Number(req.params.id) }, {
+    //   applicants: req.body.applicantId 
+    // });
+    const { nModified } = await Job.updateOne({ id: Number(req.params.id) }, {
+      $push: { applicants: req.body.applicantId }
     });
-
+    
+    console.log({ nModified })
+    
     if (nModified === 0) {
-      res.json({
+      return res.json({
         status: 'error',
         message: 'User has already applied for this position',
       })
-    } else {
+    }
+    const userUpdate = await User.findOneAndUpdate({ id: req.body.applicantId }, {
+      $push: { appliedIds: req.params.id } 
+    })
+
+    console.log({ userUpdate })
+
+    if (userUpdate) {
       res.json({
         status: 'success',
         message: 'Application sucessful',
       });
     }
+    
+     
+    
     
   });
 
