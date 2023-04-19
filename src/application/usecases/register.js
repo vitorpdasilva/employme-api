f4b58d56e804ad248c10b8a6393d2a358d33e23b
Vitor Boccio <vitorboccio@gmail.com>
2023-04-18 23:40:08 -0700

    saving name

--- src/application/usecases/register.js
+++ src/application/usecases/register.js
@@ -12,6 +12,7 @@ async function Register(userRepository, { email, password, name }) {
   
   // TODO: check this. If we remove this line, Mongo will throw an error because the id is empty and it's required to save the user
   user.id = `${Math.random() * 123456}-${Math.random() * 123456}`
+  user.name = name
   const userSaved = await userRepository.register(user)
   const token = JwtService.generate(user)
   return {
