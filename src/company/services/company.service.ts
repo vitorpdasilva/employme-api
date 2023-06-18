42fbd4053cefef3890ebf5d2f34b31e10ce3aa82
Vitor Boccio <vitorboccio@gmail.com>
2023-06-17 20:42:14 -0700

    wip

--- src/company/services/company.service.ts
+++ src/company/services/company.service.ts
@@ -2,11 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { CompanyRepository } from '../repositories/company.repository';
 import { Company } from '../schemas/company.schema';
 import { CompanyDto } from '../dtos/company.dto';
+import { RegisterCompanyInputDto } from '../dtos/register-company.dto';
 @Injectable()
 export class CompanyService {
   constructor(private readonly repository: CompanyRepository) {}
 
-  public async create(company: CompanyDto): Promise<CompanyDto> {
-    return this.repository.create(company);
+  public async register(
+    companyInput: RegisterCompanyInputDto,
+  ): Promise<CompanyDto> {
+    const { name, email, password } = companyInput;
   }
 }
